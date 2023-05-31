c2759344ec7ca4634d2d16fd27ccd5a216e22725
yahyamokaied <yahyamokaiedgmail.com>
2023-05-31 11:04:18 +0200

    added quit error handling

--- hangman.py
+++ hangman.py
@@ -11,34 +11,39 @@ def hangman():
     print("Let's play Hangman!")
     print('_ ' * len(random_word))
 
-    while attempts > 0:
-        guess = input(
-            "Please guess a letter of (programming language):").lower()
+    try:
+        while attempts > 0:
+            guess = input(
+                "Please guess a letter of (programming language):").lower()
 
-        if len(guess) != 1:
-            print("Please guess only one letter at a time.")
-        elif guess in guessed_letters:
-            print("You've already guessed this letter.")
-        elif guess not in random_word:
-            print("Sorry, this letter is not in the word.")
-            attempts -= 1
-        else:
-            print("Good job! That letter is in the word.")
-            guessed_letters.append(guess)
-
-        word_so_far = ""
-        for letter in random_word:
-            if letter in guessed_letters:
-                word_so_far += letter + " "
+            if len(guess) != 1:
+                print("Please guess only one letter at a time.")
+            elif guess in guessed_letters:
+                print("You've already guessed this letter.")
+            elif guess not in random_word:
+                print("Sorry, this letter is not in the word.")
+                attempts -= 1
             else:
-                word_so_far += "_ "
-        print(word_so_far)
+                print("Good job! That letter is in the word.")
+                guessed_letters.append(guess)
 
-        if "_" not in word_so_far:
-            print("Congratulations! You've won!")
-            return
+            word_so_far = ""
+            for letter in random_word:
+                if letter in guessed_letters:
+                    word_so_far += letter + " "
+                else:
+                    word_so_far += "_ "
+            print(word_so_far)
 
-    print("Sorry, you've run out of attempts. The word was " + random_word + ".")
+            if "_" not in word_so_far:
+                print("Congratulations! You've won!")
+                return
+
+        print("Sorry, you've run out of attempts. The word was " + random_word + ".")
+
+    except KeyboardInterrupt:
+        print("\nGame interrupted. You've chosen to quit.")
+        return
 
 
 hangman()
